a11c26559222675591d168ad0b78bb1d72ff22cd
Joey226611 <157316686+Joey226611@users.noreply.github.com>
2026-01-01 17:49:21 +0100

    Update Supabase client initialization

--- js/supabase.js
+++ js/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm";
 
-const supabase = createClient(
-  "https://JOUW_PROJECT_ID.supabase.co",
-  "JOUW_PUBLIC_ANON_KEY"
+export const supabase = createClient(
+  "https://whxdmxwxzvnsraygohfl.supabase.co",
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndoeGRteHd4enZuc3JheWdvaGZsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjcyNzg3MjksImV4cCI6MjA4Mjg1NDcyOX0.Jt0hOkW4G2rkaqbSSGQXAfIjAHjRLn_7y1K7KUY955o"
 );
